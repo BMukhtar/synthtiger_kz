3710400b6ff5e53095ca11dfac6ad3a1d573a39f
Mukhtar Bimurat <bimurat.mukhtar@gmail.com>
2023-08-29 09:47:38 +0600

    adding oscar corpus generation

---
+++ oscar/generate_oskar_corpus.py
@@ -0,0 +1,14 @@
+import collections
+
+def count_chars(filename, chunk_size=1024*1024):
+    char_count = collections.Counter()
+    with open(filename, 'r', encoding='utf-8', errors='ignore') as f:
+        while chunk := f.read(chunk_size):
+            char_count.update(chunk)
+    return char_count
+
+filename = 'kk.txt'
+char_count = count_chars(filename)
+
+# Print the 10 most common characters and their counts
+print(char_count.most_common(10))
